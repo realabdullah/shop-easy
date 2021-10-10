fe234b44c8aedf02d19671957086ccb1f334f524
realabdullah <abdulodesanmi@gmail.com>
2021-10-10 23:31:05 +0100

    env var

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://lvxtnhawypcwdyffjisb.supabase.co'
-const supabaseAnonKey = process.env.SUPABASE_KEY
+const supabaseAnonKey = import.meta.env.VUE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

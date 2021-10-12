378d384c8d8e8b43de7a432a0bf2aa5b88f578ee
realabdullah <abdulodesanmi@gmail.com>
2021-10-12 17:01:03 +0100

    .

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lvxtnhawypcwdyffjisb.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzI3NzUzMSwiZXhwIjoxOTQ4ODUzNTMxfQ.7J_nqG-xYsZYRkr9ATPJRgStomrbG3QU_iquw2hLtes'
+const supabaseUrl = process.env.VUE_APP_URL
+const supabaseAnonKey = process.env.VUE_APP_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

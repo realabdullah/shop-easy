ab9090c0b00c514f1eb889884755950dec3f1588
realabdullah <abdulodesanmi@gmail.com>
2021-10-28 15:36:27 +0100

    updates

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -8,7 +8,7 @@
             <img :src="product.file" alt="headset">
           </router-link>
             <div class="details">
-              <p class="price">${{ product.price.toLocaleString() }}</p>
+              <p class="price"><span> &#8358; </span>{{ product.price.toLocaleString() }}</p>
               <p class="name">{{ product.name }}</p>
               <div class="buttons">
                 <button class="view">

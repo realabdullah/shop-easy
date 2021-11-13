49a5bedd4c20d953351e53f0b407476d5e4cd64c
realabdullah <abdulodesanmi@gmail.com>
2021-11-13 13:47:05 +0100

    auth

--- src/router/index.js
+++ src/router/index.js
@@ -103,9 +103,15 @@ router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some
   (record => record.meta.requiresAuth)
 
-  if(requiresAuth && !currentUser) next('/sign-in')
-  else if(!requiresAuth && currentUser) next("/");
-  else next();
+  if(requiresAuth && !currentUser) {
+    next('/sign-in')
+  }
+  else if(!requiresAuth && currentUser) {
+    next("/");
+  }
+  else {
+    next();
+  }
 })
 
 export default router

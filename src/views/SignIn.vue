d1db0b4f0c616550ae9d3c7c429baf09958bac96
realabdullah <abdulodesanmi@gmail.com>
2021-11-16 07:04:40 +0100

    loaders

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -55,7 +55,25 @@
                   <p class="ssolink">Incorrect login details!</p>
                 </div>
                 <div class="field padding-bottom--24">
-                  <button class="submit">Sign In</button>
+                  <button class="submi" v-if="loading">
+                    <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
+                      <g fill="none" fill-rule="evenodd">
+                        <g transform="translate(1 1)" stroke-width="2">
+                          <circle stroke-opacity=".5" cx="18" cy="18" r="18"/>
+                          <path d="M36 18c0-9.94-8.06-18-18-18">
+                            <animateTransform
+                                attributeName="transform"
+                                type="rotate"
+                                from="0 18 18"
+                                to="360 18 18"
+                                dur="1s"
+                                repeatCount="indefinite"/>
+                          </path>
+                        </g>
+                    </g>
+                  </svg>
+                  </button>
+                  <button class="submit" v-else>Sign In</button>
                 </div>
               </form>
             </div>
@@ -81,14 +99,19 @@ export default {
     const router = useRouter()
     const email = ref('')
     const password = ref('')
+    const loading = ref(false)
     const incorrect = ref(false)
 
     const signIn = async () => {
       try {
+        loading.value = true
+        console.log(loading.value)
         const { user, session, error } = await supabase.auth.signIn({
           email: email.value,
           password: password.value
         })
+        loading.value = false
+        console.log(loading.value)
         if(user && session) {
           router.push('/')
           console.log(session)
@@ -117,6 +140,7 @@ export default {
       email,
       password,
       signIn,
+      loading,
       incorrect
     }
   }
@@ -124,6 +148,28 @@ export default {
 </script>
 
 <style scoped>
+.submi {
+  padding: 5px 16px;
+  width: 100%;
+  min-height: 44px;
+  border: unset;
+  border-radius: 4px;
+  background-color: #353030;
+  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, 
+              rgba(0, 0, 0, 0) 0px 0px 0px 0px, 
+              rgba(0, 0, 0, 0.12) 0px 1px 1px 0px, 
+              #353030 0px 0px 0px 1px, 
+              rgba(0, 0, 0, 0) 0px 0px 0px 0px, 
+              rgba(0, 0, 0, 0) 0px 0px 0px 0px, 
+              rgba(60, 66, 87, 0.08) 0px 2px 5px 0px;
+  color: #fff;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.submi svg {
+  width: 25px;
+}
 a {
   color: #353030;
   text-decoration: unset;
